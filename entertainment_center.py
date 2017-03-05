d089a4d697db871c089117dca4e9fb8d0a984054
Jessie <jjmcgarry90@gmail.com>
2017-03-04 20:55:21 -0500

    revised for submission

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,28 +1,31 @@
 import media
 import fresh_tomatoes
 
-
-# Create a list of movies to be passed to an HTML page maker
+#this function will create movie objects and then a list that contains them
 def create_movie_list():
     the_sixth_sense = media.Movie("The Sixth Sense", "A psychiatrist attempts "
-                                  "to help a child who claims to see the dead",
-                                  "https://www.youtube.com/watch?v=VG9AGf66tXM",  # NOQA
+                                  "to help a child who claims to see the dead.",
+                                  "https://www.youtube.com/watch?v=VG9AGf66tXM",
                                   "http://images6.fanpop.com/image/photos/36100000/The-Sixth-Sense-image-the-sixth-sense-36136397-1000-1500.jpg")  # NOQA
 
-    finding_dory = media.Movie("Finding Dory", "The friendly but forgetful "
-                               "blue fish begins a search for her parents.",
-                               "https://www.youtube.com/watch?v=MKJA-VLpiCo",
-                               "http://cdn.collider.com/wp-content/uploads/2015/11/finding-dory-poster.jpg")  # NOQA
+    men_in_black = media.Movie("Men in Black", "A police officer joins a secret "
+                               "organization that polices and monitors "
+                               "extraterrestrial interactions on Earth.",
+                               "https://www.youtube.com/watch?v=HYUd7AOw_lk",
+                               "http://assets.flicks.co.nz/images/movies/poster/c9/c96c08f8bb7960e11a1239352a479053_500x735.jpg")  # NOQA
 
     groundhog_day = media.Movie("Groundhog Day", "A weatherman finds himself "
-                                "living the same day over and over",
+                                "inexplicably living the same day over and over"
+                                "again.",
                                 "https://www.youtube.com/watch?v=tSVeDx9fk60",
-                                "http://www.impawards.com/1993/posters/groundhog_day.jpg")  # NOQA
+                                "http://www.impawards.com/1993/posters/groundhog_day.jpg")
     # Create a movie list that can be accessed from outside this function
     global movies
-    movies = [the_sixth_sense, finding_dory, groundhog_day]
+    movies = [the_sixth_sense, men_in_black, groundhog_day]
 
 
 create_movie_list()
-# Pass our list to the magic page-maker
+# This function call passes the movie list to an html page maker.
 fresh_tomatoes.open_movies_page(movies)
+
+
--- media.py
+++ media.py
@@ -1,15 +1,20 @@
 import webbrowser
-import fresh_tomatoes
-
-"""This class holds infomation about movies"""
-
 
 class Movie():
-    def __init__(self, title, summary, trailer_youtube_url, poster_image_url):
+    """This class holds information about movies"""
+    def __init__(self, title, storyline, trailer_youtube_url, poster_image_url):
         self.title = title
-        self.summary = summary
+        self.storyline = storyline
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = poster_image_url
 
+    
     def show_trailer(self):
         webbrowser.open(trailer_youtube_url)
+
+
+
+
+
+
+
